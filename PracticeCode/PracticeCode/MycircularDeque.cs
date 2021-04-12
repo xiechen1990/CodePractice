c9d9672a0956312f824453f9453802a751bab89c
xiechen <2581089511@qq.com>
2021-04-12 22:58:22 +0800

    提交第一周作业

---
+++ PracticeCode/PracticeCode/MycircularDeque.cs
@@ -0,0 +1,104 @@
+﻿using System;
+namespace homework1
+{
+    public class MycircularDeque
+    {
+        private int _front;
+        private int _tail;
+        private int _dequeLen;
+        private int[] _arr;
+
+
+        public MycircularDeque(int k)
+        {
+            _dequeLen = k + 1;
+            _arr = new int[_dequeLen];
+
+            _front = 0;
+            _tail = 0;
+        }
+
+        public bool InsertFront(int value)
+        {
+            if(!IsFull())
+            {
+                this._arr[_front] = value;
+                this._front = (_front - 1 ) % _dequeLen;
+                return true;
+            }
+            return false;
+        }
+
+        /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+        public bool InsertLast(int value)
+        {
+            if(!IsFull())
+            {
+                this._arr[_tail] = value;
+                this._tail = (_tail + 1) % _dequeLen;
+                return true;
+            }
+            return false;
+
+        }
+
+        /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+        public bool DeleteFront()
+        {
+            if(!IsFull())
+            {
+                _front = (_front + 1) % _dequeLen;
+                return true;
+            }
+
+            return false;
+
+        }
+
+        /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+        public bool DeleteLast()
+        {
+            if(!IsFull())
+            {
+                _tail = (_tail - 1) % _dequeLen;
+                return true;
+            }
+            return false;
+
+        }
+
+        /** Get the front item from the deque. */
+        public int GetFront()
+        {
+            if (!IsEmpty())
+            {
+                return this._arr[this._front];
+            }
+            return -1;
+
+        }
+
+        /** Get the last item from the deque. */
+        public int GetRear()
+        {
+            if(!IsEmpty())
+            {
+                return this._arr[this._tail];
+            }
+            return -1;
+        }
+
+        /** Checks whether the circular deque is empty or not. */
+        public bool IsEmpty()
+        {
+            return this._tail == this._front;
+
+        }
+
+        /** Checks whether the circular deque is full or not. */
+        public bool IsFull()
+        {
+           return this._tail + 1 % _dequeLen == this._front;
+        }
+    }
+}
--- PracticeCode/PracticeCode/Program.cs
+++ PracticeCode/PracticeCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PracticeCode
 {
@@ -6,62 +7,185 @@ namespace PracticeCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            int a = fib(5);
-            int b = fib2(5);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-        }
-
-        static int find(int[] array,int val)
-        {
-            int startindex = 0;
-            int endindex = array.Length - 1;
-            while(endindex>startindex)
-            {
-                int mid = (startindex+endindex)/ 2;
-                if(val == array[mid])
-                {
-                    return array[mid];
-                }
-                else if(val > array[mid])
-                {
-                    startindex = mid + 1;
-                }
-                else if(val < array[mid])
-                {
-                    endindex = mid - 1;
-                }
-            }
-                return -1;
-         }
-
-
-        static int fib(int n)
-        {
-
-            if(n==0)
-            {
-                return 1;
-            }
-
-            return n * fib(n - 1);
+            plusOne(new int[1] { 9});
         }
 
 
-        static int fib2(int n)
+
+        public static Array plusOne(int[] digits)
         {
-            if (n == 0)
+            for(int i = digits.Length - 1; i>= 0; i --)
             {
-                return 1;
+                if(digits[i] == 9)
+                {
+                    digits[i] = 0;
+                }
+                else
+                {
+                    digits[i] += 1;
+                    return digits;
+                }
             }
-            int sum = 1 ;
-            for(int i=1;i<=n;i++)
-            {
-                sum = sum * i;
-            }
-            return sum;
+            digits = new int[digits.Length + 1];
+            digits[0] = 1;
+            return digits;
+
         }
+
+
+
+
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if(nums == null || nums.Length == 0) return 0;
+            int i = 0;
+            int j = 1;
+            while( j < nums.Length)
+            {
+                
+                if(nums[i] != nums[j])
+                {
+                    if(i-j>1)
+                    {
+                        nums[i + 1] = nums[j];
+                        i++;
+                    }
+                }
+                j++;
+            }
+
+            return i + 1;
+        }
+
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+
+        public static ListNode MergeLink(ListNode l1, ListNode l2)
+        {
+            if(l1 == null)
+            {
+                return l2;
+            }
+            else if(l2 == null)
+            {
+                return l1;
+            }
+            else if(l1.val > l2.val)
+            {
+                l2.next = MergeLink(l1,l2.next);
+                return l2;
+            }
+            else if(l1.val < l2.val)
+            {
+                l1.next = MergeLink(l1.next, l2);
+                return l1;
+            }
+            return null;
+          
+        }
+
+
+
+        public static void movezros(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    for (int j = i; j < nums.Length - 1; j++)
+                    {
+                        nums[j] = nums[j + 1];
+                    }
+                    nums[nums.Length - 1] = 0;
+                    i = 0;
+                }
+            }
+        }
+
+
+
+        public static void MergeArray2(int[] nums1, int m, int[] nums2, int n)
+        {
+            for(int i = 0; i< n-1; i++)
+            {
+                nums1[m + i] = nums2[i];
+            }
+            Array.Sort(nums1);
+        }
+
+        public static void MergeArray(int[] nums1, int m, int[] nums2, int n)
+        {
+            int len1 = m - 1;
+            int len2 = n - 1;
+            int len = m +n - 1;
+
+            while(len1 >= 0 && len2 >= 0)
+            {
+                nums1[len--] = nums2[len2] > nums1[len1] ? nums2[len2--] : nums1[len1--];
+            }
+            
+             Array.Copy(nums2, 0, nums1,0,len2+1);
+        }
+
+
+
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int value;
+                if (dict.TryGetValue(target - nums[i], out value))
+                {
+                    return new int[] { value, i };
+                }
+                else
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+            return null;
+
+        }
+
+
+
+       public static void Rotatenums(int[] nums,int k)
+        {
+            if (nums.Length < k) k = nums.Length;
+            swapArr(nums, 0,nums.Length - 1);
+            swapArr(nums, 0, k-1);
+            swapArr(nums, k, nums.Length - 1);
+        }
+
+
+        public static void swapArr(int[] arr,int start,int end)
+        {
+            while(start<end)
+            {
+                int temp;
+                temp = arr[end];
+                arr[start++] = arr[end];
+                arr[end--] = temp;
+            }
+
+        }
+        
+
+
+      
     }
 }
